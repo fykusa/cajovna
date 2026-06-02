813880a9f9ef35dd61abe3568ad49123d060abef
fykusa <fykusa@gmail.com>
2026-06-02 17:12:02 +0200

    feat(toast): Wrap app in ToastProvider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,11 @@
 // frontend/src/App.tsx
 import AppRouter from './router/AppRouter'
+import { ToastProvider } from './components/toast/ToastProvider'
 
 export default function App() {
-  return <AppRouter />
+  return (
+    <ToastProvider>
+      <AppRouter />
+    </ToastProvider>
+  )
 }
